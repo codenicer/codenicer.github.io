9e560012fe3eeaeb4f7673bafab820e00f0865a3
codenicer <ruthertenido@gmail.com>
2021-03-06 08:19:57 +0800

    add services info title

--- components/section-header.js
+++ components/section-header.js
@@ -1,9 +1,18 @@
 import styles from '../styles/section-header.module.scss'
 
-export default function SectionHeader({ imgSrc, title, section }) {
+export default function SectionHeader({
+  imgSrc,
+  title,
+  section,
+  customImageStyle,
+}) {
   return (
     <section className={styles.header}>
-      <img className={styles.header_img} src={imgSrc} />
+      <img
+        className={styles.header_img}
+        style={customImageStyle}
+        src={imgSrc}
+      />
       <h1 className={styles.header_title}>{title}</h1>
       <h5 className={styles.header_subtitle}>
         Home {'>'} {section} {'>'}
--- pages/services/index.js
+++ pages/services/index.js
@@ -20,10 +20,33 @@ export default function Services() {
       </header>
       <main className={styles.container}>
         <SectionHeader
+          customImageStyle={{
+            transform: 'scale(.8)',
+          }}
           imgSrc={'/assets/services.svg'}
           title={'Our Services'}
           section={'Services'}
         />
+        <section className={styles.services_info_cont}>
+          <h1 className={styles.services_title}>Our Services</h1>
+          <p className={styles.services_title}>
+            WQA is a reputable management system provider, worldwide. We at
+            Worldwide Quality Assurance (WQA) provide quality services to our
+            clients through internationally recognized accredited certifications
+            for the ideal Quality Standards. WQA is your partner in making
+            management system better and effective through its value-added
+            assessments. Our work ethics are built on fundamental respects for
+            customer satisfaction. We have the strength of experienced and
+            calibrated professionals across the countries. Our products are
+            Management System Certification ISO 9001, ISO 14001, OHSAS 18001,
+            ISO 13485, ISO 22000, HACCP, GMP , ISO 22301, ISO 27001, ISO 50001,
+            ISO 37001,2nd Party Audit, Independent Supplier/Vendor Assessment.
+            We can provide Training like as IRCA & IEMA International Lead
+            Auditor Courses (ISO 9001, ISO 14001, OHSAS 18001, ISO 22000, ISO
+            27001, GMP, HACCP, ISO 37001 & ISO 50001). And also provide other
+            training that are related to ISO and Process Improvements.
+          </p>
+        </section>
         <Footer />
       </main>
     </>
