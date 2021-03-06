10d8e9c9906a29aa7077609d56a134756df2bf4a
codenicer <ruthertenido@gmail.com>
2021-03-06 12:21:07 +0800

    add contact form on contact

--- pages/contact_us/index.js
+++ pages/contact_us/index.js
@@ -1,5 +1,5 @@
 import Head from 'next/head'
-import styles from '../../styles/about.module.scss'
+import styles from '../../styles/contact-us.module.scss'
 import MobileNavbar from '../../components/mobile-navbar'
 import SectionHeader from '../../components/section-header'
 import Footer from '../../components/footer'
@@ -27,6 +27,60 @@ export default function ContactUs() {
           title={'Contact Us'}
           section={'Contact Us'}
         />
+        <section className={styles.contact_us_info_container}>
+          <ul className={styles.contact_us_info_list}>
+            <li className={styles.contact_us_info_address}>
+              <img src="/assets/location_contact.svg" />
+              <p>
+                <strong>Address:</strong> 198 West 21th Street, Suite 721 New
+                York NY 10016
+              </p>
+            </li>
+            <li className={styles.contact_us_phone}>
+              <img src="/assets/phone_contact.svg" />
+              <p>
+                <strong>Phone:</strong> + 1235 2355 98
+              </p>
+            </li>
+
+            <li className={styles.contact_us_email}>
+              <img src="/assets/contact_email.svg" />
+              <p>
+                <strong>Email:</strong>info@yoursite.com
+              </p>
+            </li>
+
+            <li className={styles.contact_us_website}>
+              <img src="/assets/contact_web.svg" />
+              <p>
+                <strong>Website:</strong> yoursite.com
+              </p>
+            </li>
+          </ul>
+        </section>
+        <section className={styles.contact_form_container}>
+          <form className={styles.contact_form}>
+            <h1 className={styles.contact_form_title}> Contact Us</h1>
+            <div className={styles.form_group}>
+              <label htmlFor="fullname">FULL NAME</label>
+              <input id="fullname" placeholder="Name" />
+            </div>
+            <div className={styles.form_group}>
+              <label htmlFor="email_address">EMAIL ADDRESS</label>
+              <input id="email_address" placeholder="Email" />
+            </div>
+            <div className={styles.form_group}>
+              <label htmlFor="subject">SUBJECT</label>
+              <input id="subject" placeholder="Subject" />
+            </div>
+            <div className={styles.form_group}>
+              <label htmlFor="message">MESSAGE</label>
+              <textarea id="message" placeholder="Message"></textarea>
+            </div>
+            <button>Send Message</button>
+          </form>
+        </section>
+        <section className={styles.contact_map}></section>
         <Footer />
       </main>
     </>
