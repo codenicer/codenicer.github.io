18e13f8f560af7fab4154ac2ad09ec5650662a57
codenicer <ruthertenido@gmail.com>
2021-03-07 14:55:43 +0800

    remove cta button on carousel

--- components/carousel-section.js
+++ components/carousel-section.js
@@ -2,12 +2,26 @@ import styles from '../styles/home.module.scss'
 import { generalSetting } from '../settings/general'
 import CarouselIndicator from './carousel-indicator'
 import Carousel from 're-carousel'
-import Link from 'next/link'
+import { useRouter } from 'next/router'
 
 export default function CarouselSection() {
+  const router = useRouter()
+
+  // const handleClick = (e) => {
+  //   e.preventDefault()
+  //   router.push('/contact_us')
+  // }
+
+  // const buttonn = () => {
+  //   return (
+  //     <button onClick={handleClick} className={styles.hero_cont__headline_cta}>
+  //       lets go
+  //     </button>
+  //   )
+  // }
   return (
     <section className={styles.carousel_container}>
-      <Carousel loop auto interval={8000} widgets={[CarouselIndicator]}>
+      <Carousel loop auto widgets={[CarouselIndicator]}>
         {generalSetting.carousel.map((content, i) => {
           return (
             <>
@@ -26,13 +40,16 @@ export default function CarouselSection() {
                 <h1 className={styles.hero_cont__headline}>
                   {content.headline}
                 </h1>
-                {content.button ? (
-                  <button className={styles.hero_cont__headline_cta}>
+                {/* {content.button ? (
+                  <button
+                    onClick={handleClick}
+                    className={styles.hero_cont__headline_cta}
+                  >
                     {content.button}
                   </button>
                 ) : (
                   <></>
-                )}
+                )} */}
               </div>
             </>
           )
